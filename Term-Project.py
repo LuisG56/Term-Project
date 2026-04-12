49ba7f58b8336ea725f3d04bc04b92020027f312
LuisG56 <lg062696@gmail.com>
2026-04-12 15:52:47 -0500

    Update Term-Project.py
    update code

--- Term-Project.py
+++ Term-Project.py
@@ -1,7 +1,22 @@
-# This is where we code!!!
-print("Hello")
+import random
+import time
 
-# Binary Tree Structure with memoization
-class Binary_Tree_Structure:
+# So this will be Part 1: The data structure
+class CarNode:
+    def __init__(self, model, price):
+        self.model = model
+        self.price = price
+        self.left = None
+        self.right = None
+        self.substree_sum = price # This is the memoization: This stores the sum of this node and all children
+
+class CarInventoryBST:
     def __init__(self):
+        self.root = None
+
+    def insert(self, model, price):
+        if self.root is None:
+            self.root = CarNode(model, price)
+        else:
+            self.insert_recursive(self.root, model, price):
         
